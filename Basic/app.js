01990d0912aecd772dacec5639624319a2c9ac59
satyam kumar mishra <satyamkmishraa@gmail.com>
2026-01-09 01:52:01 +0530

    first commit

---
+++ Basic/app.js
@@ -0,0 +1,5 @@
+var userName = "Satyam";
+var age = 30;
+var car = "black";
+console.log("Username is ".concat(userName, " is ").concat(age, " year's old"));
+console.log(car);
---
+++ Basic/app.ts
@@ -0,0 +1,6 @@
+var userName:string = "Satyam"
+var age:number = 30;
+var car:string = "black"
+
+console.log(`Username is ${userName} is ${age} year's old`)
+console.log(car);
